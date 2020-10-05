a0c45584123c8391789a082b686cc6e8fee26c31
et <et@et.com>
2020-10-05 18:41:52 +0800

    在公司开发签到功能20%

---
+++ SignIn.java
@@ -0,0 +1,12 @@
+/*
+* @Author: Marte
+* @Date:   2020-10-05 18:15:58
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-10-05 18:41:01
+*/
+
+public class SignIn {
+    private int id;
+    private String name;
+
+}
